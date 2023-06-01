551478e821128dcb2769644d05ac6a46c5258f0d
Ekaterina Scherbakova <kat-m1@mail.ru>
2023-06-01 16:55:22 +0500

    Add class task 27

--- seminar4_python/Task27.py
+++ seminar4_python/Task27.py
@@ -6,6 +6,7 @@
 # shore shells
 # Output: 13
 
+
 text = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells".replace(".", " ").split() 
 set_result = set()
 for i in text: set_result.add(i.lower()) 
